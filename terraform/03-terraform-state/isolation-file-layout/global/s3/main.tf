171dc9be78536452119fa5b9d714735868885f79
cadenhong <caden.p.hong@gmail.com>
2023-02-23 22:16:05 -0500

    isolation of environments using file layout

---
+++ terraform/03-terraform-state/isolation-file-layout/global/s3/main.tf
@@ -0,0 +1,76 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+##### TERRAFORM BLOCK #####
+
+# Defining a backend configuration to the Terraform block
+terraform {
+  # backend "s3" {
+  #   ### Since bucket, region, dynamodb_table, and encrypt will be reused, we can store this in backend.hcl
+  #   ### Must run `terraform init -backend-config=backend.hcl`
+  #     bucket = "terraform-up-and-running-follow-along-state"
+  #     region = "us-east-2"
+  #     dynamodb_table = "terraform-up-and-running-locks"
+  #     encrypt = true # We enabled this in the S3 bucket itself, but this is a second layer to ensure data is always encrypted
+  #  key = "global/s3/terraform.tfstate" # Filepath within the S3 bucket where the Terraform state file should be written; must be unique for every Terraform module deployed
+  # }
+}
+
+##### S3 BUCKET #####
+
+# Remote backend using S3 bucket
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-up-and-running-follow-along-state"
+
+  # Adding this for the destroy step
+  force_destroy = true
+  
+  # Prevent accidental deletion of this S3 bucket
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+# Enables versioning so every update to file creates a new version of the file (useful for reverting to older versions)
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Enables server-side encryption for all data written to S3 to secure any secrets are encrypted on disk when stored in S3
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# Block all public access to S3
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.terraform_state.id
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
+
+##### DYNAMO DB #####
+
+# DynamoDB table used for locking - distributed key-value store that supports consistent reads and conditional writes
+# The table's primary key MUST be "LockID"
+resource "aws_dynamodb_table" "terraform_locks" {
+  name = "terraform-up-and-running-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
---
+++ terraform/03-terraform-state/isolation-file-layout/stage/services/webserver-cluster/main.tf
@@ -0,0 +1,172 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+terraform {
+  # backend "s3" {
+  #     bucket = "terraform-up-and-running-follow-along-state"
+  #     region = "us-east-2"
+  #     dynamodb_table = "terraform-up-and-running-locks"
+  #     encrypt = true # We enabled this in the S3 bucket itself, but this is a second layer to ensure data is always encrypted
+  #     key = "stage/services/webserver-cluster/terraform.tfstate" # Same folder path as the web server Terraform code
+  # }
+}
+
+# Security Group for EC2 created
+resource "aws_security_group" "instance" {
+  name = var.server_name
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+##### AUTO SCALING GROUP #####
+### Instead of having one EC2, we will have a cluster of EC2s using Auto Scaling Group
+
+# Launch configuration to specify how to configure each EC2 in an Auto Scaling Group
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-0fb653ca2d3203ac1" # Equivalent to aws_instance.example.ami
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id] # Equivalent to aws_instance.example.vpc_security_group_ids
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+
+  lifecycle {
+    create_before_destroy = true # Required when using launch configuration with ASG for replacement EC2 to be made before destroying old ones
+  }
+}
+
+# Auto Scaling Group resource
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier  = data.aws_subnets.default.ids # Specifies which VPC subnets the EC2 should be deployed to; pulls subnet ids out of aws_subnets data source and tells ASG to use those subnets
+
+  target_group_arns = [aws_lb_target_group.asg.arn] # ALB target group attached
+  health_check_type = "ELB" # Default is EC2, but doing ELB will give a more robust health check; will also instruct ASG to replace an EC2 if it's down or serving requests due to memory outage or process crash
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+##### DATA SOURCES #####
+### Read-only info fetched from the provider (e.g. AWS); does not create anything new
+### Just queries provider's API for data to make it available to rest of the Terraform code
+
+# Data source to look up data for my default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Data source to look up subnets within default VPC
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+##### APPLICATION LOAD BALANCER #####
+### To distribute traffic across servers and give all users the IP (or DNS name) of the ALB
+### Consists of a listener, listener rule, and target group
+
+# Create an Application Load Balancer (ALB)
+resource "aws_lb" "example" {
+  name = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets = data.aws_subnets.default.ids
+  security_groups  = [aws_security_group.alb.id]
+}
+
+# Listener -> To specify port (e.g. 80) and protocol (e.g. HTTP) to listen on
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port = 80
+  protocol = "HTTP"
+
+  # Return a simple 404 page as default response for requests that don't match any listener rules
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code = 404
+    }
+  }
+}
+
+# Security Group for ALB to allow incoming requests on port 80 so people can access the ALB over HTTP and allow outgoing requests on all ports so the ALB can perform health checks
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+
+  # Allow inbount HTTP requests
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound requests
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Target Group -> one or more servers that receives requests from the ALB
+# Also performs health checks and sends requests only to healthy nodes
+# Attach this to the aws_autoscaling_group resource by pointing target_group_arns to this
+resource "aws_lb_target_group" "asg" {
+  name = "terraform-asg-example"
+  port = var.server_port
+  protocol = "HTTP"
+  vpc_id = data.aws_vpc.default.id
+
+  # This TG will health check the EC2s by periodically sending an HTTP request to
+  # each instance and will consider the instance "healthy" only if it returns a
+  # response that matches the configured "matcher"; if it is marked "unhealthy",
+  # the TG will automatically stop sending traffic to it to minimize disruptions for users
+  health_check {
+    path = "/"
+    protocol = "HTTP"
+    matcher = "200" # Must match this for an EC2 to be considered healthy
+    interval = 15
+    timeout = 3
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+  }
+}
+
+# Listener Rule -> Takes requests that come into a listener and sends those that match specific paths or hostnames to target groups
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority = 100
+
+  condition {
+    path_pattern {
+      values = ["*"] # To match any path to the target group that contains the ASG
+    }
+  }
+
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
